21303ddc17ffb096646249e7f7afdf971d0ad26e
CHUSUEYEON <rmffkfk226@naver.com>
2025-03-06 21:25:37 +0900

    [feat] SWEA.5215 햄버거 문제 풀이

---
+++ src/Algorithm_Study/daily/CSY/D20250306_햄버거.java
@@ -0,0 +1,11 @@
+package Algorithm_Study.daily.CSY;
+
+import java.util.Scanner;
+
+public class D20250306_햄버거 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        String s = "";
+    }
+}
