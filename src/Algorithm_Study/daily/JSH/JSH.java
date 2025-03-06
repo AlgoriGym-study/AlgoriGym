5842ea1bbd60c262f0aadd33123186cb6c1ab08d
jayji5204 <jayji5204@naver.com>
2025-03-06 21:19:30 +0900

    수박수박수박 / 야구 팀 구성

--- src/Algorithm_Study/daily/JSH/JSH.java
+++ src/Algorithm_Study/daily/JSH/JSH.java
@@ -1,67 +1,49 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class JSH {
+class JSH {
+	public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) throws Exception {
-//		Scanner sc = new Scanner(System.in);
-        Scanner sc = new Scanner(new File("input.txt"));
+		Scanner sc = new Scanner(System.in);
+//		Scanner sc = new Scanner(new File("input.txt"));
 
-        int T=sc.nextInt();
-        sc.nextLine();
+		int T = sc.nextInt();
+		sc.nextLine();
 
-        for (int test_case = 1; test_case <= T; test_case++) {
+		for (int test_case = 1; test_case <= T; test_case++) {
 
-            int N = sc.nextInt();
-            sc.nextLine();
+			int N = sc.nextInt();
+			int K = sc.nextInt();
 
-            char[][] board = new char[N][N];
+			int[] arr = new int[N];
 
-            for (int i = 0; i < N; i++) {
-                board[i] = sc.nextLine().toCharArray();
-            }
+			for (int i = 0; i < N; i++) {
+				arr[i] = sc.nextInt();
+			}
 
-            boolean answer = false;
+			Arrays.sort(arr);
+			int maxCount = -1;
+			int count = 0;
 
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (board[i][j] == 'o') {
-                        // 1. 가로 방향
-                        if (j + 4 < N && board[i][j + 1] == 'o' && board[i][j + 2] == 'o' && board[i][j + 3] == 'o'
-                                && board[i][j + 4] == 'o') {
-                            answer = true;
-                        }
-                        // 2. 세로 방향
-                        if (i + 4 < N && board[i + 1][j] == 'o' && board[i + 2][j] == 'o' && board[i + 3][j] == 'o'
-                                && board[i + 4][j] == 'o') {
-                            answer = true;
-                        }
-                        // 3. 대각선 검사
-                        if (i + 4 < N && j + 4 < N && board[i + 1][j + 1] == 'o' && board[i + 2][j + 2] == 'o'
-                                && board[i + 3][j + 3] == 'o' && board[i + 4][j + 4] == 'o') {
-                            answer = true;
-                        }
-                        // 4. 대각선 검사
-                        if (i + 4 < N && j - 4 >= 0 && board[i + 1][j - 1] == 'o' && board[i + 2][j - 2] == 'o'
-                                && board[i + 3][j - 3] == 'o' && board[i + 4][j - 4] == 'o') {
-                            answer = true;
-                        }
-
-
-                    }
-                }
-
-            }
-            String result;
-            if(answer==true) {
-                result="Yes";
-            }else {
-                result="No";
-            }
-
-            System.out.println("#" + test_case + " " +result);
-        }
-    }
-
+			for (int i = 0; i < N; i++) {
+				for (int j = N - 1; j > i; j--) {
+					// K가 0이면 1을 반환
+					if (K == 0) {
+						maxCount = 1;
+					}
+					// 최대값 - 최소값의 차이가 2 이내인 경우
+					if (arr[j] - arr[i] <= K) {
+						// 최대값의 인덱스번호인 j와
+						// 최소값의 인덱스번호인 i를 빼주고
+						// 자기자신은 항상 포함이기에 1을 더해준다
+						count = j - i + 1;
+						maxCount = Math.max(maxCount, count);
+					}
+				}
+			}
+			System.out.println(maxCount);
+		}
+	}
 }
