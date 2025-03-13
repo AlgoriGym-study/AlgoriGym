f60bdb91bab34886ba3c77faa547a88b267a3295
Jgone2 <jgoneit@gmail.com>
2025-03-13 21:13:00 +0900

    [feat] 백준 11726번 2xn 타일링 문제 풀이(머지로 인한 재커밋..)

--- src/Algorithm_Study/daily/SJG/D20250313.java
+++ src/Algorithm_Study/daily/SJG/D20250313.java
@@ -12,6 +12,7 @@ public class D20250313 {
         dp[1] = 1;
         dp[2] = 2;
         
+        // N이 3이상일떄 dp[N] = dp[N-1] + dp[N-2] 라는 규칙을 발견
         for(int i = 3; i <= N; i++) {
             dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
         }
