ac0d7c1837b53a7a4e7daf3a34264c3c2ae8e8bf
Jgone2 <jgoneit@gmail.com>
2025-03-17 22:02:58 +0900

    [SJG] #0006 N-Quuen 공통 문제 풀이

--- src/Algorithm_Study/daily/JSH/D20250224.java
+++ src/Algorithm_Study/daily/JSH/D20250224.java
@@ -1,46 +1,46 @@
-
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-class D20250224 {
-
-    public int[] solution(int[] arr) {
-
-        List<Integer> list = new ArrayList<>();
-
-        int[] answer = {};
-
-        if (arr.length == 1) {
-            answer = new int[1];
-            answer[0] = -1;
-            return answer;
-        } else {
-            int[] checkMin = new int[arr.length];
-            int[] checkArr = new int[arr.length];
-            for (int i = 0; i < arr.length; i++) {
-                checkMin[i] = arr[i];
-                checkArr[i] = arr[i];
-                list.add(arr[i]);
-            }
-            Arrays.sort(checkMin);
-            int deleteNum = checkMin[0];
-            int idx = 0;
-            for (int i = 0; i < checkMin.length; i++) {
-                if (checkArr[i] == deleteNum) {
-                    idx = i;
-                }
-            }
-            list.remove(idx);
-            answer = new int[list.size()];
-            for (int i = 0; i < list.size(); i++) {
-                answer[i] = list.get(i);
-            }
-            return answer;
-
-        }
-
-    }
-
-}
+//
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//class D20250224 {
+//
+//    public int[] solution(int[] arr) {
+//
+//        List<Integer> list = new ArrayList<>();
+//
+//        int[] answer = {};
+//
+//        if (arr.length == 1) {
+//            answer = new int[1];
+//            answer[0] = -1;
+//            return answer;
+//        } else {
+//            int[] checkMin = new int[arr.length];
+//            int[] checkArr = new int[arr.length];
+//            for (int i = 0; i < arr.length; i++) {
+//                checkMin[i] = arr[i];
+//                checkArr[i] = arr[i];
+//                list.add(arr[i]);
+//            }
+//            Arrays.sort(checkMin);
+//            int deleteNum = checkMin[0];
+//            int idx = 0;
+//            for (int i = 0; i < checkMin.length; i++) {
+//                if (checkArr[i] == deleteNum) {
+//                    idx = i;
+//                }
+//            }
+//            list.remove(idx);
+//            answer = new int[list.size()];
+//            for (int i = 0; i < list.size(); i++) {
+//                answer[i] = list.get(i);
+//            }
+//            return answer;
+//
+//        }
+//
+//    }
+//
+//}
