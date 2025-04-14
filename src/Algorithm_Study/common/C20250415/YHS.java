e4a362dbe8857deef0f7ef7ed6df14f933ac6030
dbscjstk3 <dbscjstk3@naver.com>
2025-04-14 23:17:22 +0900

    #0014 나무베기 문제풀이

---
+++ src/Algorithm_Study/common/C20250415/YHS.java
@@ -0,0 +1,12 @@
+package Algorithm_Study.common.C20250415;
+
+import java.util.*;
+import java.io.*;
+
+public class YHS {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        
+    }
+}
