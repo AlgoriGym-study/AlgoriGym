3071276f6735a830bc6a79d4387366ee344ce44f
dbscjstk3 <dbscjstk3@gmail.com>
2025-05-04 21:12:29 +0900

    백준 단축키지정 && 지름길 문제풀이

--- src/Algorithm_Study/daily/YHS/D20250501_단축키지정.java
+++ src/Algorithm_Study/daily/YHS/D20250501_단축키지정.java
@@ -1,40 +1,71 @@
 package Algorithm_Study.daily.YHS;
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 public class D20250501_단축키지정 {
-    public int[] solution(String[] genres, int[] plays) {
-        Map<String, Integer> map = new HashMap<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Set<Character> registered = new HashSet<>();
 
-        for (int i = 0; i < genres.length; i++) {
-            map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
-        }
+        pointA : for(int now = 0 ; now < N ; now++){
+            String[] inputArr = br.readLine().split(" ");
 
-        List<String> list = new ArrayList<>(map.keySet());
-        Collections.sort(list, (o1,o2) -> map.get(o2) - map.get(o1));
+            // 1. 단어의 첫 글자가 단축키로 지정되었는지 확인한다.
+            for(int i = 0 ; i < inputArr.length ; i++){
+                // 지정되지 않은 것이 있을 경우
+                if(!registered.contains(inputArr[i].charAt(0))){
+                    // set에 등록
+                    registered.add(Character.toUpperCase(inputArr[i].charAt(0)));
+                    registered.add(Character.toLowerCase(inputArr[i].charAt(0)));
 
-        List<Song> songList = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            String genre = list.get(i);
+                    // 현재 단어의 첫 번째 글자에 괄호를 씌워준다.
+                    StringBuilder sb = new StringBuilder(inputArr[i]);
+                    sb.insert(0, '[');
+                    sb.insert(2, ']');
+                    inputArr[i] = sb.toString();
 
-            for (int j = 0; j < genres.length; j++) {
-
+                    // 출력 후 다음 입력으로 ㄱㄱ
+                    printAnswer(inputArr);
+                    continue pointA;
+                }
             }
+
+            // 2. 모든 단어의 첫 글자가 이미 지정되어 있다면, 왼쪽에서부터 차례대로 본다.
+            for(int i = 0 ; i < inputArr.length ; i++){
+                for(int j = 0 ; j < inputArr[i].length() ; j++){
+                    if(!registered.contains(inputArr[i].charAt(j))){
+                        // set에 등록
+                        registered.add(Character.toUpperCase(inputArr[i].charAt(j)));
+                        registered.add(Character.toLowerCase(inputArr[i].charAt(j)));
+
+                        // 현재 글자에 괄호를 씌워준다.
+                        StringBuilder sb = new StringBuilder(inputArr[i]);
+                        sb.insert(j, '[');
+                        sb.insert(j+2, ']');
+                        inputArr[i] = sb.toString();
+
+                        // 출력 후 다음 입력으로 ㄱㄱ
+                        printAnswer(inputArr);
+                        continue pointA;
+                    }
+                }
+            }
+
+            printAnswer(inputArr);
         }
     }
 
-    static class Song implements Comparable<Song> {
-        String genre;
-        int play;
-
-        public Song(String genre, int play) {
-            this.genre = genre;
-            this.play = play;
+    public static void printAnswer(String[] arr){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < arr.length ; i++){
+            if(i == arr.length - 1) sb.append(arr[i]);
+            else sb.append(arr[i]).append(" ");
         }
-
-        @Override
-        public int compareTo(Song o) {
-            return this.play - o.play;
-        }
+        System.out.println(sb.toString());
     }
 }
