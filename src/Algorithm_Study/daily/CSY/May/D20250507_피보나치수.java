7fac4007db0720141032954a7b29a199b61e1d46
CHUSUEYEON <rmffkfk226@naver.com>
2025-05-07 14:12:08 +0900

    [feat] 프로그래머스 피보나치수 문제풀이

--- src/Algorithm_Study/daily/CSY/May/D20250507_피보나치수.java
+++ src/Algorithm_Study/daily/CSY/May/D20250507_피보나치수.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public class D20250507_피보나치수 {
     public static void main(String[] args) {
-
+        solution(10);
     }
     static int[] memo;
 
-    public int solution(int n) {
+    public static int solution(int n) {
         memo = new int[n+1];
         Arrays.fill(memo, -1);
         memo[0] = 0;
@@ -18,7 +18,7 @@ public class D20250507_피보나치수 {
         return answer;
     }
 
-    public int fibo(int n){
+    public static int fibo(int n){
         if(memo[n] == -1){
             memo[n] = (fibo(n-2) + fibo(n-1)) % 1234567;
         }
