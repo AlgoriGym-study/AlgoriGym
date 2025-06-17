b1a712a6756618335cc602921d83dd9ebca79b26
youngwoo2 <152594318+youngwoo2@users.noreply.github.com>
2025-06-17 23:37:15 +0900

    [LYW] 0022, 0023 문제 재풀이

---
+++ src/Algorithm_Study/common/C20250611/LYW0023_2.java
@@ -0,0 +1,50 @@
+package Algorithm_Study.common.C20250611;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LYW0023_2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T = sc.nextInt();
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int N = sc.nextInt();
+			int[][] arr = new int[N][N];
+
+			int K = N;
+			int r = 0;
+			int c = -1;
+			int dir = 1;
+			int num = 1;
+
+			while (true) {
+				for (int i = 0; i < K; i++) {
+					c += dir;
+					arr[r][c] = num++;
+				}
+				K--;
+				if (K == 0)
+					break;
+
+				for (int i = 0; i < K; i++) {
+					r += dir;
+					arr[r][c] = num++;
+				}
+				dir = -dir;
+			}
+
+			System.out.println("#" + test_case);
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					System.out.print(arr[i][j] + " ");
+				}
+				System.out.println();
+			}
+
+		}
+	}
+
+}
