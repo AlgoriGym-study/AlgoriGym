39b0fd99e93bd0b775ca257d3b77279ef1d81fae
CHUSUEYEON <rmffkfk226@naver.com>
2025-06-24 22:52:50 +0900

    [CSY] 0024, 0025 복습문제 풀이 & 0026, 0027 공통문제 풀이

---
+++ src/Algorithm_Study/common/C20250618/CSY0024_2.java
@@ -0,0 +1,66 @@
+package Algorithm_Study.common.C20250618;
+
+import java.util.Arrays;
+
+public class CSY0024_2 {
+    static int[] discount = {10, 20, 30, 40}; // 이모티콘 할인율 배열
+    static int people = 0; // 최대 구독자 수
+    static int revenue = 0; // 최대 매출액
+
+    public static void main(String[] args) {
+        int[][] users = {{40, 10000}, {25, 10000}};
+        int[] emotions = {7000, 9000};
+
+        int[] dis = new int[emotions.length]; // 각 이모티콘에 대한 할인율 저장
+        dfs(0, dis, users, emotions); // 모든 할인율 조합 탐색
+
+        int[] answer = {people,revenue};
+        System.out.println(Arrays.toString(answer));
+    }
+
+    // dfs 조합 완탐
+    static void dfs(int dp, int[] dis, int[][] users, int[] emotions){
+        if(dp == emotions.length){
+            eva(dis, users, emotions);
+            return;
+        }
+
+        for(int rate : discount){
+            dis[dp] = rate; // 각 이모티콘별로 4개의 할인율 적용한 것을 재귀 탐색
+            dfs(dp+1, dis, users, emotions);
+        }
+    }
+
+    // 해당 할인율 조합에 대한 가입자 수와 매출 계산
+    static void eva(int[] dis, int[][] users, int[] emotions){
+        int p = 0; // 가입자 수
+        int r = 0; // 매출
+
+        for(int[] user : users){
+            int min = user[0]; // 최소 할인율 조건
+            int change = user[1]; // 구독 전환 기준
+            int sum = 0;
+
+            for(int i = 0; i < emotions.length; i++){
+                int rate = dis[i];
+                if(rate >= min){
+                    sum += emotions[i] * (100 - rate) / 100;
+                }
+            }
+
+            if(sum >= change){ // 구입 총액이 전환 기준 넘으면 구독자 수 증가.
+                p++;
+            }else{
+                r += sum;
+            }
+
+            if(p > people){ // 만약 이번 조합이 기존보다 높으면 갱신
+                people = p;
+                revenue = r;
+            }else if(p == people){ // 구독자수가 같으면 매출액 중 더 높은 걸로 갱신
+                revenue = Math.max(r, revenue);
+            }
+
+        }
+    }
+}
---
+++ src/Algorithm_Study/common/C20250618/CSY0025_2.java
@@ -0,0 +1,43 @@
+package Algorithm_Study.common.C20250618;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSY0025_2 {
+    static int res;
+
+    public static void main(String[] args) {
+        int n = 10;
+        int[][] q = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {3, 7, 8, 9, 10},{2, 5, 7, 9, 10}, {3, 4, 5, 6, 7}};
+        int[] ans = {2, 3, 4, 3, 3};
+
+        res = 0;
+        List<Integer> code = new ArrayList<>();
+        dfs(1, code, n, q, ans);
+
+        System.out.println(res);
+    }
+
+    static void dfs(int start, List<Integer> code, int n, int[][] q, int[] ans){
+        if(code.size() == 5){ // 서로 다른 정수 5개가 모이면 검사
+            if(validate(code, q, ans)) res++;
+            return;
+        }
+        for(int i = start; i <=n; i++){
+            code.add(i);
+            dfs(i+1, code, n, q, ans);
+            code.remove(code.size() -1);
+        }
+    }
+
+    static boolean validate(List<Integer> code, int[][] q, int[] ans){
+        for(int i = 0; i < q.length; i++){ // 각 q별로 ans의 수와 맞는지 체크
+            int cnt = 0;
+            for(int x : q[i]){
+                if(code.contains(x)) cnt++;
+            }
+            if(cnt != ans[i]) return false;
+        }
+        return true;
+    }
+}
