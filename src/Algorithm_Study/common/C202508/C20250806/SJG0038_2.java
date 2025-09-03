481a04a234c7f570a21d542fdbc013572dbea6d7
Jgone2 <jgoneit@gmail.com>
2025-09-03 12:39:22 +0900

    [SJG] 0038 큰 수 만들기

--- src/Algorithm_Study/common/C202508/C20250806/SJG0038_2.java
+++ src/Algorithm_Study/common/C202508/C20250806/SJG0038_2.java
@@ -1,24 +1,24 @@
 package Algorithm_Study.common.C202508.C20250806;
 
 public class SJG0038_2 {
-    public static void main(String[] args) {
-        String s = "111222333444555666777888999000";
-        int result = findLargestCoolNum(s);
-        System.out.println(result); // Expected output: 999
-    }
-    
-    public static int findLargestCoolNum(String s) {
-    int max = -1;
-    if (s.length() < 3) return -1;
-
-    for (int i = 0; i + 2 < s.length(); i++) { // 마지막 윈도우 포함
-        char c = s.charAt(i);
-        if (c == s.charAt(i + 1) && c == s.charAt(i + 2)) {
-            int val = (c - '0') * 111; // "000" -> 0, "777" -> 777
-            if (val == 999) return 999; // 최댓값이면 즉시 종료
-            if (val > max) max = val;
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (char c : number.toCharArray()) {
+            // StringBuilder의 마지막 문자와 비교하여 제거
+            while (k > 0 && sb.length() > 0 && sb.charAt(sb.length() - 1) < c) {
+                sb.deleteCharAt(sb.length() - 1);
+                k--;
+            }
+            sb.append(c);
         }
+        
+        // 남은 k개 제거
+        while (k > 0) {
+            sb.deleteCharAt(sb.length() - 1);
+            k--;
+        }
+        
+        return sb.toString();
     }
-    return max;
-}
 }
