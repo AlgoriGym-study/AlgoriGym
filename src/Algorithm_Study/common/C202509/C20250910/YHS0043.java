11108054cf7eba99036d04040ef0588d02ee05e7
dbscjstk3 <dbscjstk3@gmail.com>
2025-09-10 17:11:19 +0900

    0042 && 0043 문제 풀이

---
+++ src/Algorithm_Study/common/C202509/C20250910/YHS0043.java
@@ -0,0 +1,15 @@
+package Algorithm_Study.common.C202509.C20250910;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class YHS0043 {
+    static int N, M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+    }
+}
