34c57a25187cde16420c402a908953441688a72f
Yerin Lee <rt8632@naver.com>
2025-12-05 02:01:41 +0900

    [LYR] 프로그래머스 최댓값과 최솟값 풀이

---
+++ src/Algorithm_Study/daily/LYR/D20251204.java
@@ -0,0 +1,20 @@
+package Algorithm_Study.daily.LYR;
+
+import java.util.StringTokenizer;
+
+public class D20251204 {
+    public String solution(String s) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        StringTokenizer st = new StringTokenizer(s);
+        while(st.hasMoreTokens()){
+            int num = Integer.parseInt(st.nextToken());
+            if(num > max)
+                max = num;
+            if(min > num)
+                min = num;
+        }
+        String answer = min + " " + max;
+        return answer;
+    }
+}
