5f4d1b7b0eb4ba30fef0fded264ec8a3673c2c7d
Yerin Lee <rt8632@naver.com>
2025-12-10 23:56:13 +0900

    [LYR] 프로그래머스 나머지가 1이 되는 수 찾기 풀이

---
+++ src/Algorithm_Study/daily/LYR/D20251210.java
@@ -0,0 +1,14 @@
+package Algorithm_Study.daily.LYR;
+
+public class D20251210 {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=2;i<n;i++){
+            if(n%i == 1){
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
