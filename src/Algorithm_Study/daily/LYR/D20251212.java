a9b1ee726144ea6f58923c011668826ce6b8bbf9
Yerin Lee <rt8632@naver.com>
2025-12-13 01:18:13 +0900

    [LYR] 프로그래머스 문자열을 정수로 바꾸기 풀이

---
+++ src/Algorithm_Study/daily/LYR/D20251212.java
@@ -0,0 +1,8 @@
+package Algorithm_Study.daily.LYR;
+
+public class D20251212 {
+    public int solution(String s) {
+        int answer = Integer.parseInt(String.valueOf(s));
+        return answer;
+    }
+}
