907d7fb4878877b96e678c8ec9341e3cfd0cfc27
Jayeun Park <jayeunpark0704@gmail.com>
2025-12-17 00:19:27 +0900

    [PJE] 프로그래머스 flag에 따라 다른 값 반환하기 믄제풀이

---
+++ src/Algorithm_Study/daily/PJE/D202512/D20251216.java
@@ -0,0 +1,12 @@
+package Algorithm_Study.daily.PJE.D202512;
+public class D20251216 {
+    public int solution(int a, int b, boolean flag) {
+        int answer = 0;
+        if(flag)
+           answer=a+b;
+        else
+            answer=a-b;
+      
+        return answer;
+    }
+}
