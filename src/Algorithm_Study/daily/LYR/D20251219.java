ca7c753dd91ef33ca855f6cbb7e1c4c6e321fb3f
Yerin Lee <rt8632@naver.com>
2025-12-20 00:16:40 +0900

    [LYR] 프로그래머스 문자열 내 p와 y의 개수 풀이

--- src/Algorithm_Study/daily/LYR/D20251219.java
+++ src/Algorithm_Study/daily/LYR/D20251219.java
@@ -1,4 +1,24 @@
 package Algorithm_Study.daily.LYR;
 
 public class D20251219 {
+    boolean solution(String s) {
+        boolean answer = true;
+        int p = 0;
+        int y = 0;
+        char[] ss = s.toCharArray();
+        for(char c : ss){
+            if(c == 'p' || c == 'P'){
+                p++;
+            } else if(c == 'y' || c == 'Y'){
+                y++;
+            }
+        }
+
+        if(p == y)
+            answer = true;
+        else
+            answer = false;
+
+        return answer;
+    }
 }
