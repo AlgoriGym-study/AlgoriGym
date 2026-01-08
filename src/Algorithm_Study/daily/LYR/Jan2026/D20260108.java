9cec495bb9f93b0e77d22912ff26e002a3eb0adb
Yerin Lee <rt8632@naver.com>
2026-01-08 17:05:14 +0900

    [LYR] 프로그래머스 자연수 뒤집어 배열로 만들기 풀이

--- src/Algorithm_Study/daily/LYR/Jan2026/D20260108.java
+++ src/Algorithm_Study/daily/LYR/Jan2026/D20260108.java
@@ -1,5 +1,7 @@
 package Algorithm_Study.daily.LYR.Jan2026;
 
+import java.util.Stack;
+
 public class D20260108 {
     public boolean solution(int x) {
         String[] numList = String.valueOf(x).split("");
@@ -11,5 +13,19 @@ public class D20260108 {
             return true;
         else
             return false;
+    }
+
+    public int[] solution(long n) {
+        String[] list = String.valueOf(n).split("");
+        Stack<String> stack = new Stack<>();
+        for(String s : list){
+            stack.push(s);
+        }
+        int[] answer = new int[list.length];
+        for(int i=0;i<answer.length;i++){
+            answer[i] = Integer.parseInt(stack.pop());
+        }
+
+        return answer;
     }
 }
