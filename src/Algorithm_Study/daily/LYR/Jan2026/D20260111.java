ae53efc1cdcdb39db4501bd806432bac2eafe969
Yerin Lee <rt8632@naver.com>
2026-01-11 21:30:12 +0900

    [LYR] 프로그래머스 없는 숫자 더하기 풀이

---
+++ src/Algorithm_Study/daily/LYR/Jan2026/D20260111.java
@@ -0,0 +1,20 @@
+package Algorithm_Study.daily.LYR.Jan2026;
+
+public class D20260111 {
+    public int solution(int[] numbers) {
+        boolean[] isExist = new boolean[10];
+        for(int i=0;i<10;i++){
+            for(int num : numbers){
+                if(i==num)
+                    isExist[i] = true;
+            }
+        }
+
+        int answer = 0;
+        for(int i=0;i<10;i++){
+            if(!isExist[i])
+                answer += i;
+        }
+        return answer;
+    }
+}
