d0d6c1cbfc4e0a3ac4cb70e989cccb29c5900ea6
youngwoo2 <152594318+youngwoo2@users.noreply.github.com>
2026-01-14 20:29:12 +0900

    [LYW] 01/14 문제풀이

---
+++ src/Algorithm_Study/daily/LYW/D2026_01_14.java
@@ -0,0 +1,32 @@
+package Algorithm_Study.daily.LYW;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class D2026_01_14 {
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+         
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            st = new StringTokenizer(br.readLine(), " ");
+            Deque<Integer> dq = new ArrayDeque<>();
+            dq.add(Integer.parseInt(st.nextToken()));
+            for(int i = 1; i < 10; i++) {
+                int n = Integer.parseInt(st.nextToken());
+                if(dq.peekLast() >= n) dq.addFirst(n);
+                else dq.addLast(n);
+            }
+            sb.append("#").append(test_case).append(" ").append(dq.peekLast()).append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
