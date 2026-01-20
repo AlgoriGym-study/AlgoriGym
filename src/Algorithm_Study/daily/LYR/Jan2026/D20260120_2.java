8b8f0fde7e945c68adfae385ab5a0017dd10926a
Yerin Lee <rt8632@naver.com>
2026-01-20 22:04:48 +0900

    [LYR] 프로그래머스 PCCE 지폐접기 풀이

---
+++ src/Algorithm_Study/daily/LYR/Jan2026/D20260120_2.java
@@ -0,0 +1,24 @@
+package Algorithm_Study.daily.LYR.Jan2026;
+
+public class D20260120_2 {
+    public int solution(int[] wallet, int[] bill) {
+        int answer = 0;
+
+        while(min(bill) > min(wallet) || max(bill) > max(wallet)){
+            if(bill[0] > bill[1])
+                bill[0] = bill[0] / 2;
+            else
+                bill[1] = bill[1] / 2;
+            answer++;
+        }
+        return answer;
+    }
+
+    private int max(int[] arr){
+        return Math.max(arr[0], arr[1]);
+    }
+
+    private int min(int[] arr){
+        return Math.min(arr[0], arr[1]);
+    }
+}
