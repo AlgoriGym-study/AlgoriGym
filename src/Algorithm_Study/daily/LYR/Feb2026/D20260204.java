9c601af399a52f2ccc5afd93ccf479cf0116955f
Yerin Lee <rt8632@naver.com>
2026-02-04 15:13:01 +0900

    [LYR] 프로그래머스 제일 작은 수 제거 풀이

---
+++ src/Algorithm_Study/daily/LYR/Feb2026/D20260204.java
@@ -0,0 +1,25 @@
+package Algorithm_Study.daily.LYR.Feb2026;
+
+public class D20260204 {
+    public int[] solution(int[] arr) {
+        if(arr.length == 1){
+            return new int[]{-1};
+        }
+        int[] answer = new int[arr.length-1];
+        int idx = 0;
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(min > arr[i]){
+                min = arr[i];
+                idx = i;
+            }
+        }
+        int index = 0;
+        for(int i=0;i<arr.length;i++){
+            if(i == idx)
+                continue;
+            answer[index++] = arr[i];
+        }
+        return answer;
+    }
+}
