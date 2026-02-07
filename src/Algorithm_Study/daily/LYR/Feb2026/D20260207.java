82cdf236c4a6426a7c1a9ca674ba5d048fddabf8
Yerin Lee <rt8632@naver.com>
2026-02-07 14:11:12 +0900

    [LYR] 프로그래머스 직사각형 별찍기 풀이

--- src/Algorithm_Study/daily/LYR/Feb2026/D20260207.java
+++ src/Algorithm_Study/daily/LYR/Feb2026/D20260207.java
@@ -22,4 +22,13 @@ public class D20260207 {
         }
         return answer;
     }
+
+    public static void star(int a, int b){
+        for(int i=0;i<b;i++){
+            for(int j=0;j<a;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
