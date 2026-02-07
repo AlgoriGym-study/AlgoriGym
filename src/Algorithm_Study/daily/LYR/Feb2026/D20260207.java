82d509b04a1841894c043ebc8f5add2adbc837c1
Yerin Lee <rt8632@naver.com>
2026-02-07 14:35:20 +0900

    [LYR] 프로그래머스 같은 숫자는 싫어 풀이

--- src/Algorithm_Study/daily/LYR/Feb2026/D20260207.java
+++ src/Algorithm_Study/daily/LYR/Feb2026/D20260207.java
@@ -1,5 +1,7 @@
 package Algorithm_Study.daily.LYR.Feb2026;
 
+import java.util.Stack;
+
 public class D20260207 {
     public boolean solution(String s) {
         boolean answer = true;
@@ -30,5 +32,33 @@ public class D20260207 {
             }
             System.out.println();
         }
+    }
+
+    public int[] solution(int []arr) {
+        Stack<Integer> array = new Stack<>();
+        Stack<Integer> result = new Stack<>();
+        for(int a : arr){
+            array.push(a);
+        }
+
+        while(!array.isEmpty()){
+            if(result.isEmpty()){
+                result.push(array.peek());
+                array.pop();
+            }
+            if(result.peek() == array.peek())
+                array.pop();
+            else {
+                result.push(array.peek());
+                array.pop();
+            }
+        }
+        int[] answer = new int[result.size()];
+        for(int i=0;i<answer.length;i++){
+            answer[i] = result.peek();
+            result.pop();
+        }
+
+        return answer;
     }
 }
