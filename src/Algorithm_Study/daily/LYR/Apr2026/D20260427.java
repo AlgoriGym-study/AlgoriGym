1705525b021049fb51fde84bad469aa71d6d3e00
Yerin Lee <rt8632@naver.com>
2026-04-27 22:32:20 +0900

    [LYR] 프로그래머스 두 수의 나눗셈 풀이

---
+++ src/Algorithm_Study/daily/LYR/Apr2026/D20260427.java
@@ -0,0 +1,8 @@
+package Algorithm_Study.daily.LYR.Apr2026;
+
+public class D20260427 {
+    public int solution(int num1, int num2) {
+        double answer = (double)num1 / (double)num2 * 1000;
+        return (int)answer;
+    }
+}
