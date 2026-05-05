eb61eb0838b78f043c0c7b58f22e16b8ad9e38e3
Yerin Lee <rt8632@naver.com>
2026-05-05 20:22:19 +0900

    [LYR] 프로그래머스 두 수의 합 구하기 풀이

---
+++ src/Algorithm_Study/daily/LYR/May2026/D20260505.java
@@ -0,0 +1,8 @@
+package Algorithm_Study.daily.LYR.May2026;
+
+public class D20260505 {
+    public int solution(int num1, int num2) {
+        int answer = num1 + num2;
+        return answer;
+    }
+}
