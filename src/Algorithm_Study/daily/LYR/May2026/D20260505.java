9d698fb38de8b349126577df4e7272a45be56b7a
Yerin Lee <rt8632@naver.com>
2026-05-05 20:23:42 +0900

    [LYR] 프로그래머스 피자 나눠 먹기(1) 풀이

--- src/Algorithm_Study/daily/LYR/May2026/D20260505.java
+++ src/Algorithm_Study/daily/LYR/May2026/D20260505.java
@@ -5,4 +5,13 @@ public class D20260505 {
         int answer = num1 + num2;
         return answer;
     }
+
+    public int solution(int n) {
+        int answer = 0;
+        if(n % 7 == 0)
+            answer = n/7;
+        else
+            answer = n/7 + 1;
+        return answer;
+    }
 }
