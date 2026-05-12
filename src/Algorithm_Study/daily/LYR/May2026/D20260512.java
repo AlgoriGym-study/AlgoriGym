8a93115dc8fa2e422dae08a149781363946381e6
Yerin Lee <rt8632@naver.com>
2026-05-12 19:43:11 +0900

    [LYR] 프로그래머스 아이스 아메리카노 풀이

--- src/Algorithm_Study/daily/LYR/May2026/D20260512.java
+++ src/Algorithm_Study/daily/LYR/May2026/D20260512.java
@@ -11,4 +11,10 @@ public class D20260512 {
         return answer;
     }
 
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        answer[0] = money / 5500;
+        answer[1] = money % 5500;
+        return answer;
+    }
 }
