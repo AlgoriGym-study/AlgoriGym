4e99a4460304835ffd8ac055591d97e2f803ec33
Yerin Lee <rt8632@naver.com>
2026-05-19 21:14:20 +0900

    [LYR] 프로그래머스 추억 점수 풀이

--- src/Algorithm_Study/daily/LYR/May2026/D20250519.java
+++ src/Algorithm_Study/daily/LYR/May2026/D20250519.java
@@ -44,4 +44,21 @@ public class D20250519 {
             answer = nums.length/2;
         return answer;
     }
+
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        HashMap<String, Integer> score = new HashMap<>();
+        for(int i=0;i<name.length;i++){
+            score.put(name[i], yearning[i]);
+        }
+        for(int i=0;i<photo.length;i++){
+            for(String s : photo[i]){
+                if(score.get(s) == null)
+                    answer[i] += 0;
+                else
+                    answer[i] += score.get(s);
+            }
+        }
+        return answer;
+    }
 }
