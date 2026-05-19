5a23de7e5b262becf9d6e4c1415584ab43a838a1
Yerin Lee <rt8632@naver.com>
2026-05-19 17:01:05 +0900

    [LYR] 프로그래머스 K번째수 풀이

--- src/Algorithm_Study/daily/LYR/May2026/D20250519.java
+++ src/Algorithm_Study/daily/LYR/May2026/D20250519.java
@@ -1,11 +1,28 @@
 package Algorithm_Study.daily.LYR.May2026;
 
+import java.util.Arrays;
+
 public class D20250519 {
     public int solution(int a, int b, int n) {
         int answer = 0;
         while(n >= a){
             answer += n/a*b;
             n = n - (n/a)*a + (n/a)*b;
+        }
+        return answer;
+    }
+
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for(int x = 0;x<commands.length;x++){
+            int[] arr = new int[commands[x][1] - commands[x][0] + 1];
+            int idx = 0;
+            for(int i=commands[x][0]-1;i<=commands[x][1]-1;i++){
+                arr[idx] = array[i];
+                idx++;
+            }
+            Arrays.sort(arr);
+            answer[x] = arr[commands[x][2]-1];
         }
         return answer;
     }
