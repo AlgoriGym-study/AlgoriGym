fe2ac7623130297f9ffae351c0fc6b59388c449c
김미림 <155730596+marinRim@users.noreply.github.com>
2026-05-26 22:50:26 +0900

    [KMR] 20260526 외계어 사전

---
+++ src/Algorithm_Study/daily/KMR/D20260526_PRO_외계어사전_Lv0.java
@@ -0,0 +1,25 @@
+package Algorithm_Study.daily.KMR;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class D20260526_PRO_외계어사전_Lv1 {
+    public int solution(String[] spell, String[] dic) {
+        Set<Character> spellSet = new HashSet<>();
+        for (String s : spell) {
+            spellSet.add(s.charAt(0));
+        }
+
+        for (String word : dic) {
+            Set<Character> wordSet = new HashSet<>();
+            for (char c : word.toCharArray()) {
+                wordSet.add(c);
+            }
+            if (wordSet.equals(spellSet)) {
+                return 1;
+            }
+        }
+
+        return 2;
+    }
+}
