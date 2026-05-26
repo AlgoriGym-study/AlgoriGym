0d9874c83f561924ddf0640e7e7c36ee4bf58da8
Yerin Lee <rt8632@naver.com>
2026-05-26 20:07:37 +0900

    [LYR] 프로그래머스 짝수 홀수 개수 풀이

--- src/Algorithm_Study/daily/LYR/May2026/D20260526.java
+++ src/Algorithm_Study/daily/LYR/May2026/D20260526.java
@@ -8,4 +8,19 @@ public class D20260526 {
         }
         return answer;
     }
+
+    public int[] solution2 (int[] num_list) {
+        int[] answer = new int[2];
+        int odd = 0;
+        int even = 0;
+        for(int n : num_list){
+            if(n % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+        answer[0] = even;
+        answer[1] = odd;
+        return answer;
+    }
 }
