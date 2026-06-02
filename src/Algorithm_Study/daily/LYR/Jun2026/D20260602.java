7d6b364bcee89dccddd35143e467909dd915db28
Yerin Lee <rt8632@naver.com>
2026-06-02 18:51:14 +0900

    [LYR] 프로그래머스 특정 문자 제거하기 풀이

--- src/Algorithm_Study/daily/LYR/Jun2026/D20260602.java
+++ src/Algorithm_Study/daily/LYR/Jun2026/D20260602.java
@@ -6,4 +6,16 @@ public class D20260602 {
         sb.append(my_string).reverse();
         return sb.toString();
     }
+
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        char[] mystr = my_string.toCharArray();
+        for(char c : mystr){
+            if(String.valueOf(c).equals(letter))
+                continue;
+            answer += String.valueOf(c);
+
+        }
+        return answer;
+    }
 }
