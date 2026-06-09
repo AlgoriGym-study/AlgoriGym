85b9ad31fc1c592461860deb57c402404195ede5
Yerin Lee <rt8632@naver.com>
2026-06-09 21:29:45 +0900

    [LYR] 프로그래머스 캐시 풀이

--- src/Algorithm_Study/daily/LYR/Jun2026/D20260609.java
+++ src/Algorithm_Study/daily/LYR/Jun2026/D20260609.java
@@ -1,5 +1,9 @@
 package Algorithm_Study.daily.LYR.Jun2026;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class D20260609 {
     public int solution(int n, int k) {
         int answer = 0;
@@ -25,5 +29,35 @@ public class D20260609 {
             return 3;
         else
             return 4;
+    }
+
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        HashMap<String, Integer> cache = new HashMap<>();
+        Queue<String> queue = new LinkedList<>();
+        for(String c : cities){
+            String city = c.toLowerCase();
+            if(cache.containsKey(city)){
+                answer += 1;
+                queue.remove(city);
+                queue.add(city);
+                continue;
+            }
+            else
+                answer += 5;
+            if(cacheSize == 0)
+                continue;
+            if(queue.size() < cacheSize){
+                cache.put(city, 1);
+                queue.add(city);
+            }
+            else{
+                cache.remove(queue.peek());
+                queue.remove();
+                queue.add(city);
+                cache.put(city,1);
+            }
+        }
+        return answer;
     }
 }
