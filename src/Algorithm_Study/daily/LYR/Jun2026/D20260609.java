a084f9df49d631eb35b3073516708c31a492c886
Yerin Lee <rt8632@naver.com>
2026-06-09 16:38:12 +0900

    [LYR] 프로그래머스 배열 자르기 풀이

--- src/Algorithm_Study/daily/LYR/Jun2026/D20260609.java
+++ src/Algorithm_Study/daily/LYR/Jun2026/D20260609.java
@@ -6,4 +6,13 @@ public class D20260609 {
         answer = n * 12000 + k * 2000 - n/10 * 2000;
         return answer;
     }
+
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2-num1+1];
+        int idx = 0;
+        for(int i=num1;i<=num2;i++){
+            answer[idx++] = numbers[i];
+        }
+        return answer;
+    }
 }
