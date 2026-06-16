08fc7d212c3ca7288d8b256011c0201bb7534bee
Yerin Lee <rt8632@naver.com>
2026-06-16 20:29:30 +0900

    [LYR] 프로그래머스 배열 원소의 길이 풀이

--- src/Algorithm_Study/daily/LYR/Jun2026/D20260616.java
+++ src/Algorithm_Study/daily/LYR/Jun2026/D20260616.java
@@ -24,4 +24,12 @@ public class D20260616 {
         }
         return sb.toString();
     }
+
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        for(int i=0;i<strlist.length;i++){
+            answer[i] = strlist[i].length();
+        }
+        return answer;
+    }
 }
