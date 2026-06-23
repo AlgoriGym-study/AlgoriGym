18b470349ca68d0152ce65f958d6b835b5ade356
Yerin Lee <rt8632@naver.com>
2026-06-23 18:37:36 +0900

    [LYR] 프로그래머스 짝수는 싫어요 풀이

---
+++ src/Algorithm_Study/daily/LYR/Jun2026/D20260623.java
@@ -0,0 +1,18 @@
+package Algorithm_Study.daily.LYR.Jun2026;
+
+public class D20260623 {
+    public int[] solution(int n) {
+        int size = 0;
+        if(n % 2 == 1)
+            size = n/2 + 1;
+        else
+            size = n/2;
+        int[] answer = new int[size];
+        int idx = 0;
+        for(int i=1;i<=n;i++){
+            if(i % 2 == 1)
+                answer[idx++] = i;
+        }
+        return answer;
+    }
+}
