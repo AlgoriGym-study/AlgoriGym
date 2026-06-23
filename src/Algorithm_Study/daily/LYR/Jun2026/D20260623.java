d81f6c88c2f77b9b5c09a0bc844eaeafc0a49e76
Yerin Lee <rt8632@naver.com>
2026-06-23 18:43:52 +0900

    [LYR] 프로그래머스 삼각형의 완성조건 (1) 풀이

--- src/Algorithm_Study/daily/LYR/Jun2026/D20260623.java
+++ src/Algorithm_Study/daily/LYR/Jun2026/D20260623.java
@@ -15,4 +15,36 @@ public class D20260623 {
         }
         return answer;
     }
+
+    public int solution(int[] sides) {
+        int answer = 0;
+        int max = 0;
+        int idx = 0;
+        for(int i=0;i<3;i++){
+            if(max < sides[i]){
+                max = sides[i];
+                idx = i;
+            }
+        }
+
+        switch(idx){
+            case 0:
+                if(sides[1] + sides[2] <= max)
+                    return 2;
+                else
+                    return 1;
+            case 1:
+                if(sides[0] + sides[2] <= max)
+                    return 2;
+                else
+                    return 1;
+            case 2:
+                if(sides[0] + sides[1] <= max)
+                    return 2;
+                else
+                    return 1;
+            default:
+                return 0;
+        }
+    }
 }
