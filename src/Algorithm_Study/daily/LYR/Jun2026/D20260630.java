4d67954acd2ae95abe887eaff07f7f3df9c68e71
Yerin Lee <rt8632@naver.com>
2026-06-30 16:31:57 +0900

    [LYR] 프로그래머스 편지 풀이

---
+++ src/Algorithm_Study/daily/LYR/Jun2026/D20260630.java
@@ -0,0 +1,9 @@
+package Algorithm_Study.daily.LYR.Jun2026;
+
+public class D20260630 {
+    public int solution(String message) {
+        int answer = 0;
+        answer = message.length() * 2;
+        return answer;
+    }
+}
