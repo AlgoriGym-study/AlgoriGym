81a6cf1b1f3f77400c43b114d7861bf77bcdc018
Yerin Lee <rt8632@naver.com>
2026-06-30 16:35:42 +0900

    [LYR] 프로그래머스 n의 배수 고르기 풀이

--- src/Algorithm_Study/daily/LYR/Jun2026/D20260630.java
+++ src/Algorithm_Study/daily/LYR/Jun2026/D20260630.java
@@ -1,9 +1,22 @@
 package Algorithm_Study.daily.LYR.Jun2026;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class D20260630 {
     public int solution(String message) {
         int answer = 0;
         answer = message.length() * 2;
+        return answer;
+    }
+
+    public int[] solution(int n, int[] numlist) {
+        List<Integer> list = new ArrayList<>();
+        for(int num : numlist){
+            if(num % n == 0)
+                list.add(num);
+        }
+        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
         return answer;
     }
 }
