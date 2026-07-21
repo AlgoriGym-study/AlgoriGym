c83773561e84be02a32b5346c2bad5841e4a3a0e
Yerin Lee <rt8632@naver.com>
2026-07-21 19:54:34 +0900

    [LYR] 프로그래머스 가장 큰 수 찾기 풀이

--- src/Algorithm_Study/daily/LYR/Jul2026/D20260721.java
+++ src/Algorithm_Study/daily/LYR/Jul2026/D20260721.java
@@ -9,4 +9,19 @@ public class D20260721 {
         }
         return answer;
     }
+
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        int idx = 0;
+        int max = 0;
+        for(int i=0;i<array.length;i++){
+            if(max < array[i]){
+                max = array[i];
+                idx = i;
+            }
+        }
+        answer[0] = max;
+        answer[1] = idx;
+        return answer;
+    }
 }
